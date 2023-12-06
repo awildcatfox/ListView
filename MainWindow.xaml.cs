c7e887e6dccdc02626a219e09ccf51bd3792f930
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-12-05 20:50:24 -0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<Student> students = new List<Student>();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateStudents();
+            lvDisplayList.ItemsSource = students;
+
+           
+            lvDisplayList.SelectedIndex = 0;
+        }
+
+        private void PopulateStudents()
+        {
+           
+            students.Add(new Student("John", "Doe"));
+            students.Add(new Student("Jane", "Smith"));
+            students.Add(new Student("Jotaro", "Kujo"));
+            students.Add(new Student("Ken", "Masters"));
+            students.Add(new Student("Banjo", "Kazooie"));
+          
+        } 
+
+        private void lvDisplayList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+            if (lvDisplayList.SelectedIndex != -1)
+            {
+               
+                Student selectedStudent = students[lvDisplayList.SelectedIndex];
+
+                MessageBox.Show($"Full Name: {selectedStudent._firstName} {selectedStudent._lastName}");
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView
+{
+    public class Student
+    {
+
+        public string _idNumber;
+        public string _firstName;
+        public string _lastName;
+        public string IDNumber => _idNumber;
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+        public Student(string firstName,string lastName)
+        {
+
+
+            Random rand = new Random();
+
+            _idNumber = rand.Next(100000000, 1000000000).ToString();
+
+
+
+            _firstName = firstName;
+            _lastName = lastName;
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
